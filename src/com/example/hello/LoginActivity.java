c0733d61d1e8e50093a5c0f293206504ae8ed334
Administrator <Administrator@7A304-0038>
2016-12-10 11:49:16 +0800

    账号登录

--- src/com/example/hello/LoginActivity.java
+++ src/com/example/hello/LoginActivity.java
@@ -1,10 +1,25 @@
 package com.example.hello;
 
+import java.io.IOException;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Contacts.People;
 import android.view.View;
 import inputcells.SimpleTextInputCellFragment;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okio.BufferedSink;
 
 public class LoginActivity extends Activity {
 	
@@ -66,9 +81,100 @@ public class LoginActivity extends Activity {
 		startActivity(itnt);
 	}
 	void goLogin(){
+		
+		String account=fragAccount.getText();
+		String passwordHash=fragPassword.getText();
+		
+		OkHttpClient client=new OkHttpClient();
+		
+		RequestBody requestbody=new MultipartBody.Builder()
+				.setType(MultipartBody.FORM)
+				.addFormDataPart("account", account)
+				.addFormDataPart("passwordHash",passwordHash)
+				.build();
+		
+		Request request=new Request.Builder()
+				.url("http://172.27.0.24:8080/membercenter/api/login")
+				.method("post", null)
+				.post(requestbody)
+				.build();
+			
+			final ProgressDialog progressdialog=new ProgressDialog(LoginActivity.this);
+			progressdialog.setMessage("Login...");
+			progressdialog.setCancelable(false);
+			progressdialog.setCanceledOnTouchOutside(false);
+			progressdialog.show();
+			
+			client.newCall(request).enqueue(new Callback() {
+				
+				@Override
+				public void onResponse(final Call arg0, Response arg1) throws IOException {
+					final String message =arg1.body().string();
+					runOnUiThread(new Runnable() {
+						
+						@Override
+						public void run() {
+							progressdialog.dismiss();
+							if(message.equals("")){
+								LoginActivity.this.onResponse(arg0,"密码错误！");
+							}else{
+								try{								
+									LoginActivity.this.onResponse(arg0, message);								
+								}catch(Exception e){								
+									e.printStackTrace();								
+									LoginActivity.this.onFailure(arg0, e);
+								}
+							}							
+						}
+					});
+					
+				}
+				
+				@Override
+				public void onFailure(final Call arg0, final IOException arg1) {
+					runOnUiThread(new Runnable() {
+						
+						@Override
+						public void run() {
+							progressdialog.dismiss();
+							onFailure(arg0, arg1);							
+						}
+					});
+					
+				}
+			});
+		
+		
+		
 		Intent itnt=new Intent(this,HelloWorldActivity.class);
 		startActivity(itnt);
 	}
+	
+	
+	void onResponse(Call call,String response){
+		new AlertDialog.Builder(this)
+		.setTitle("请求成功")
+		.setMessage(response)
+		.setPositiveButton("确认", new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				
+			}
+		}).show();
+			Intent itnt=new Intent(this,HelloWorldActivity.class);
+			startActivity(itnt);		
+		}
+	
+	void onFailure(Call call,Exception e){
+		new AlertDialog.Builder(this)
+		.setTitle("请求失败")
+		.setMessage(e.getLocalizedMessage())
+		.setPositiveButton("确认",null)
+		.show();
+	}
+	
 	
 	void goRecoverPassword(){
 		Intent itnt = new Intent(this, PasswordRecoverActivity.class);
